e354ec6329095148c49985507694409f6b6a2ccc
Peter Mäder <peter@maeder.info>
2016-09-29 12:51:58 +0200

    Enhanced with Firebase connect and AboutMeScreen enhancements

--- AboutMe/AboutMeViewController.swift
+++ AboutMe/AboutMeViewController.swift
@@ -18,13 +18,15 @@ class AboutMeViewController: UIViewController {
     
     @IBOutlet weak var nameLabel: UILabel!
     
+    @IBOutlet weak var messageButton: UIButton!
+    @IBOutlet weak var phoneButton: UIButton!
+    @IBOutlet weak var mailButton: UIButton!
+    
     
     // MARK: View Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -41,6 +43,38 @@ class AboutMeViewController: UIViewController {
                 }
             }
         }
+        
+        let buttons : [UIButton] = [messageButton, phoneButton, mailButton]
+        roundButtonCorners(buttons: buttons, radius: 10)
+        
     }
     
+    // MARK: Actions
+    @IBAction func sendMessage(_ sender: UIButton) {
+        print("sendMessage")
+        sender.backgroundColor = UIColor.white
+    }
+    
+    @IBAction func initiatePhoneCall(_ sender: UIButton) {
+        print("initiatePhoneCall")
+        sender.backgroundColor = UIColor.white
+    }
+    
+    @IBAction func sendMail(_ sender: UIButton) {
+        print("sendMail")
+        sender.backgroundColor = UIColor.white
+    }
+    
+    @IBAction func buttonReleased(_ sender: UIButton) {
+        sender.backgroundColor = UIColor.gray
+//        sender.backgroundColor = UIColor.init(red: CGFloat(25), green: CGFloat(87), blue: CGFloat(127), alpha: CGFloat(0.7))
+    }
+
+    // MARK: Utilities
+    private func roundButtonCorners(buttons: [UIButton], radius: Int){
+        
+        for button in buttons{
+            button.layer.cornerRadius = CGFloat(radius)
+        }
+    }
 }
